51253554a10f09e918dd4a62c053d5fd3677b012
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2023-06-25 18:29:48 -0500

    improved handling ext dr

--- vsftpd-log-listener.sh
+++ vsftpd-log-listener.sh
@@ -3,22 +3,22 @@
 #https://stackoverflow.com/questions/48645159/how-to-extract-file-name-from-vsftpd-log-with-shell-script
 #https://creativelycode.com/posts/a-linux-bash-script-to-recognize-when-a-file-is-added-to-ftp
 
-function convert_H264_to_H265 () 
+function convert_H264_to_H265 ()
 {
     H265_TS_Video="${2:0: -4}.ts" # .ts file name to save the output to .ts format at the destination path. It avoids overwriting source files.
     rm "$H265_TS_Video" || true # IF THERE IS A .ts file from an aborted conversion, delete it first. Refer https://superuser.com/questions/76061/how-do-i-make-rm-not-give-an-error-if-a-file-doesnt-exist
-    echo [ "$timestamp" ]: CONVERTING "$1" to "$H265_TS_Video" 
+    echo [ "$timestamp" ]: CONVERTING "$1" to "$H265_TS_Video"
     RESULT=$? # From https://unix.stackexchange.com/questions/22726/how-to-conditionally-do-something-if-a-command-succeeded-or-failed
     ffmpeg -i  "$1" -c:v libx265 -vtag hvc1 -loglevel quiet -x265-params log-level=quiet "$H265_TS_Video" <>/dev/null 2>&1 # ffmpeg conversion command . Quietened as in https://unix.stackexchange.com/questions/229390/bash-ffmpeg-libx265-prevent-output
     if [ $RESULT -eq 0 ]; then
-       H265_MPG_Video="${H265_TS_Video%.*}.mpg" 
+       H265_MPG_Video="${H265_TS_Video%.*}.mpg"
        echo [ "$timestamp" ]: SUCCESSFULLY converted "$1"
-       echo [ "$timestamp" ]: RENAMING "$H265_TS_Video" to  "$H265_MPG_Video" 
+       echo [ "$timestamp" ]: RENAMING "$H265_TS_Video" to  "$H265_MPG_Video"
        mv "$H265_TS_Video" "$H265_MPG_Video" # Change the file extension from .ts to .mpg in the same directory. This can be set up to send it to any directory.
        if [ $RESULT -eq 0 ]; then
            echo [ "$timestamp" ]: RENAMED "$H265_TS_Video" to MPG file "$H265_MPG_Video"
-           echo [ "$timestamp" ]: DELETING H.264 file "$1"
-           rm "$1"
+           #echo [ "$timestamp" ]: DELETING H.264 file "$1"
+           #rm "$1"
        else
            echo [ "$timestamp" ]: FAILED to RENAME "$H265_TS_Video" to MPG file "$H265_MPG_Video"
        fi
@@ -30,25 +30,24 @@ function convert_H264_to_H265 ()
 # This listener filters out successful uploads and then converts MP4 files from H.264 video codec to H.265 video codec and saves the latter as .MPG
 # It runs the conversion activity as a separate process as in https://bash.cyberciti.biz/guide/Putting_functions_in_background so that it does not hold up the tail watch for other uploaded files
 
-timestamp="$(date +"%F %T")"
+timestamp="$(date +"%F %T")" # Date format is F - Full date, T - full time
 ext_dr_mnt_pt=$1 # Mount point of external drive
 base_folder=$2 # Base folder
 
 
-
-echo [ "$timestamp" ]: BEGIN LOGGING # Date format is F - Full date, T - full time
+echo [ "$timestamp" ]: BEGIN LOGGING. DESTINATION ASKED AS "$ext_dr_mnt_pt"+"$base_folder"
 
 tail -f -s 5 -n 1 /var/log/vsftpd.log | while read log_line; do
     if echo "$log_line" | grep -q 'OK UPLOAD:'; then # If there is a successful upload
         username=$(echo "$log_line" | sed -r 's/.*?\]\s\[(.+?)\].*?$/\1/') # Find out which user uploaded
-        user_home=$(getent passwd "$username" | cut -d: -f6) # from https://superuser.com/questions/484277/get-home-directory-by-username
+        user_home=$(getent passwd "$username" | cut -d: -f6) # Get the home directory of that user. Refer https://superuser.com/questions/484277/get-home-directory-by-username
         echo [ "$timestamp" ]: USERNAME is "$username" : USER HOME is "$user_home"
-        file_at_rel_path=$(echo "$log_line" | sed -r 's/.*?\,\s\"(.+?)\".*?$/\1/') # Take everything within quotes. https://www.baeldung.com/linux/process-a-bash-variable-with-sed                     
-        rel_path=$(echo "$file_at_rel_path" | sed -r 's/(^\/.+)*\/(.+)\.(.+)$/\1/') # Take everything from the first \/ and before the last \/ character. # https://stackoverflow.com/questions/9363145/regex-for-extracting-filename-from-path
+        file_at_rel_path=$(echo "$log_line" | sed -r 's/.*?\,\s\"(.+?)\".*?$/\1/') # Take everything within quotes in the log line. https://www.baeldung.com/linux/process-a-bash-variable-with-sed
+        rel_path=$(echo "$file_at_rel_path" | sed -r 's/(^\/.+)*\/(.+)\.(.+)$/\1/') # Take everything from the first \/ and before the last \/ character in the log line. https://stackoverflow.com/questions/9363145/regex-for-extracting-filename-from-path
         echo [ "$timestamp" ]: SUCCESSFUL UPLOAD at "$user_home""$file_at_rel_path" # Log the full source path
-      
+
         if [[ "$file_at_rel_path" == *.mp4 ]]; then # If this is an mp4 file
-	    
+
 	    ## Get the path to the destination file - Begin
             destination_path="$user_home""$rel_path" # Default value if the external mount point is not mounted, or it is the same as the mount point of the users home
             destination_file="$user_home""$file_at_rel_path" # Default value if the external mount point is not mounted, or it is the same as the mount point of the users home
@@ -56,7 +55,12 @@ tail -f -s 5 -n 1 /var/log/vsftpd.log | while read log_line; do
             ext_dr_mnt_status=false
             if mountpoint -q "$ext_dr_mnt_pt"; then # Check if the external drive is already / still mounted. Mounting it is out of scope of this shell script
                 ext_dr_mnt_status=true
-            fi 
+                echo [ "$timestamp" ]: EXTERNAL DRIVE IS ALREADY / STILL MOUNTED. CREATING "$user_home""$base_folder" AND DOING mount --rbind
+                mkdir -p  "$user_home""$base_folder"  # This helps ftp clients see the base_folder on the external mount point in the root folder of the ftp user 
+                mount --rbind "$ext_dr_mnt_pt""$base_folder" "$user_home""$base_folder" # This helps ftp clients see the base_folder on the external mount point in the root folder of the ftp user
+            else
+                echo [ "$timestamp" ]: EXTERNAL DRIVE IS NOT MOUNTED
+            fi
 
             if $ext_dr_mnt_status && [[ $user_home != $ext_dr_mnt_pt ]]; then # Change destination_path if the external mount point is mounted, and is different from the mount point of the users home
 
@@ -65,13 +69,15 @@ tail -f -s 5 -n 1 /var/log/vsftpd.log | while read log_line; do
 
                 if [ -d "$ext_dr_mnt_pt""$base_folder""$rel_path" ]; then # Check if the newly created path exists before pointing variables to it
                     destination_path="$ext_dr_mnt_pt""$base_folder""$rel_path"
-                    destination_file="$ext_dr_mnt_pt""$base_folder""$file_at_rel_path" 
+                    destination_file="$ext_dr_mnt_pt""$base_folder""$file_at_rel_path"
+                else
+                    echo [ "$timestamp" ]: NOT CHANGED DESTINATION PATH OR DESTINATION FILE
                 fi
             fi
  	    ## Get the path to the destination file - End
 
- 	    
-            convert_H264_to_H265 "$user_home""$file_at_rel_path" "$destination_file" & # Run the conversion as a separate process 
+
+            convert_H264_to_H265 "$user_home""$file_at_rel_path" "$destination_file" & # Run the conversion as a separate process
         else
             echo [ "$timestamp" ]: NOT AN MP4 FILE AT "$user_home""$file_at_rel_path"
         fi
