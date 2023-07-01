d3b263dbf4b74263950ec0f9d8a3b43b11dce598
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2023-06-30 19:48:09 -0500

    timestamp

--- vsftpd-log-listener.sh
+++ vsftpd-log-listener.sh
@@ -6,64 +6,64 @@
 function convert_H264_to_H265 ()
 {
     H265_TS_Video="${2:0: -4}.ts" # .ts file name to save the output to .ts format at the destination path. It avoids overwriting source files.
-    
+
     if [ -f "$H265_TS_Video" ];then # IF THERE IS A .ts file from an aborted conversion, delete it first. https://tecadmin.net/bash-script-check-if-file-is-empty-or-not/
         if [ -s "$H265_TS_Video" ];then
-            echo [ "$timestamp" ]: File "$H265_TS_Video" exists from previous attempts and is not empty. Deleting it to start conversion afresh
+            echo [ "$(date +"%F %T")" ]: File "$H265_TS_Video" exists from previous attempts and is not empty. Deleting it to start conversion afresh
         else
-	    echo [ "$timestamp" ]: File "$H265_TS_Video" exists from previous attempts but is empty. Deleting it to start conversion afresh
+	    echo [ "$(date +"%F %T")" ]: File "$H265_TS_Video" exists from previous attempts but is empty. Deleting it to start conversion afresh
         fi
-	rm "$H265_TS_Video" 
+	rm "$H265_TS_Video"
     fi
-    
-    echo [ "$timestamp" ]: CONVERTING "$1" to "$H265_TS_Video"
+
+    echo [ "$(date +"%F %T")" ]: CONVERTING "$1" to "$H265_TS_Video"
     RESULT=$? # From https://unix.stackexchange.com/questions/22726/how-to-conditionally-do-something-if-a-command-succeeded-or-failed
     ffmpeg -i  "$1" -c:v libx265 -vtag hvc1 -loglevel quiet -x265-params log-level=quiet "$H265_TS_Video" <>/dev/null 2>&1 # ffmpeg conversion command . Quietened as in https://unix.stackexchange.com/questions/229390/bash-ffmpeg-libx265-prevent-output
     if [ $RESULT -eq 0 ]; then
         H265_MPG_Video="${H265_TS_Video%.*}.mpg"
-        echo [ "$timestamp" ]: SUCCESSFULLY converted "$1"
-        echo [ "$timestamp" ]: RENAMING "$H265_TS_Video" to  "$H265_MPG_Video"
+        echo [ "$(date +"%F %T")" ]: SUCCESSFULLY converted "$1"
+        echo [ "$(date +"%F %T")" ]: RENAMING "$H265_TS_Video" to  "$H265_MPG_Video"
         mv "$H265_TS_Video" "$H265_MPG_Video" # Change the file extension from .ts to .mpg in the same directory. This can be set up to send it to any directory.
         if [ $RESULT -eq 0 ]; then
-            echo [ "$timestamp" ]: RENAMED "$H265_TS_Video" to MPG file "$H265_MPG_Video"
+            echo [ "$(date +"%F %T")" ]: RENAMED "$H265_TS_Video" to MPG file "$H265_MPG_Video"
 	    if [ -f "$H265_MPG_Video" ];then # IF THERE IS A NON-EMPTY .mpg file, delete the mp4 file. https://tecadmin.net/bash-script-check-if-file-is-empty-or-not/
                 if [ -s "$H265_MPG_Video" ];then
-	             echo [ "$timestamp" ]: File "$H265_MPG_Video" exists and is not empty. Deleting H.264 mp4 file.
+	             echo [ "$(date +"%F %T")" ]: File "$H265_MPG_Video" exists and is not empty. Deleting H.264 mp4 file.
 	             rm "$1"
 	         else # Keep the mp4 file
-	             echo [ "$timestamp" ]: File "$H265_MPG_Video" exists but is empty. Deleting it and keeping the  H.264 mp4 file 
+	             echo [ "$(date +"%F %T")" ]: File "$H265_MPG_Video" exists but is empty. Deleting it and keeping the  H.264 mp4 file
 	             #mv "$H265_MPG_Video" to destination path
                  fi
              else # Keep the mp4 file
-	    	 echo [ "$timestamp" ]: File "$H265_MPG_Video" does not exists. Keeping the  H.264 mp4 file 
+	    	 echo [ "$(date +"%F %T")" ]: File "$H265_MPG_Video" does not exists. Keeping the  H.264 mp4 file
 	         #mv "$H265_MPG_Video" to destination path
              fi
          else
-             echo [ "$timestamp" ]: FAILED to RENAME "$H265_TS_Video" to MPG file "$H265_MPG_Video"
+             echo [ "$(date +"%F %T")" ]: FAILED to RENAME "$H265_TS_Video" to MPG file "$H265_MPG_Video"
          fi
      else
-         echo [ "$timestamp" ]: FAILED to convert "$1"
+         echo [ "$(date +"%F %T")" ]: FAILED to convert "$1"
      fi
 }
 
 # This listener filters out successful uploads and then converts MP4 files from H.264 video codec to H.265 video codec and saves the latter as .MPG
 # It runs the conversion activity as a separate process as in https://bash.cyberciti.biz/guide/Putting_functions_in_background so that it does not hold up the tail watch for other uploaded files
+# Date format in time stamp is F - Full date, T - full time
 
-timestamp="$(date +"%F %T")" # Date format is F - Full date, T - full time
 ext_dr_mnt_pt=$1 # Mount point of external drive
 base_folder=$2 # Base folder
 
 
-echo [ "$timestamp" ]: BEGIN LOGGING. DESTINATION ASKED AS "$ext_dr_mnt_pt"+"$base_folder"
+echo [ "$(date +"%F %T")" ]: BEGIN LOGGING. DESTINATION ASKED AS "$ext_dr_mnt_pt"+"$base_folder"
 
 tail -f -s 5 -n 1 /var/log/vsftpd.log | while read log_line; do
     if echo "$log_line" | grep -q 'OK UPLOAD:'; then # If there is a successful upload
         username=$(echo "$log_line" | sed -r 's/.*?\]\s\[(.+?)\].*?$/\1/') # Find out which user uploaded
         user_home=$(getent passwd "$username" | cut -d: -f6) # Get the home directory of that user. Refer https://superuser.com/questions/484277/get-home-directory-by-username
-        echo [ "$timestamp" ]: USERNAME is "$username" : USER HOME is "$user_home"
+        echo [ "$(date +"%F %T")" ]: USERNAME is "$username" : USER HOME is "$user_home"
         file_at_rel_path=$(echo "$log_line" | sed -r 's/.*?\,\s\"(.+?)\".*?$/\1/') # Take everything within quotes in the log line. https://www.baeldung.com/linux/process-a-bash-variable-with-sed
         rel_path=$(echo "$file_at_rel_path" | sed -r 's/(^\/.+)*\/(.+)\.(.+)$/\1/') # Take everything from the first \/ and before the last \/ character in the log line. https://stackoverflow.com/questions/9363145/regex-for-extracting-filename-from-path
-        echo [ "$timestamp" ]: SUCCESSFUL UPLOAD at "$user_home""$file_at_rel_path" # Log the full source path
+        echo [ "$(date +"%F %T")" ]: SUCCESSFUL UPLOAD at "$user_home""$file_at_rel_path" # Log the full source path
 
         if [[ "$file_at_rel_path" == *.mp4 ]]; then # If this is an mp4 file
 
@@ -74,23 +74,23 @@ tail -f -s 5 -n 1 /var/log/vsftpd.log | while read log_line; do
             ext_dr_mnt_status=false
             if mountpoint -q "$ext_dr_mnt_pt"; then # Check if the external drive is already / still mounted. Mounting it is out of scope of this shell script
                 ext_dr_mnt_status=true
-                echo [ "$timestamp" ]: EXTERNAL DRIVE IS ALREADY / STILL MOUNTED. CREATING "$user_home""$base_folder" AND DOING mount --rbind
+                echo [ "$(date +"%F %T")" ]: EXTERNAL DRIVE IS ALREADY / STILL MOUNTED. CREATING "$user_home""$base_folder" AND DOING mount --rbind
                 mkdir -p  "$user_home""$base_folder"  # This helps ftp clients see the base_folder on the external mount point in the root folder of the ftp user 
                 mount --rbind "$ext_dr_mnt_pt""$base_folder" "$user_home""$base_folder" # This helps ftp clients see the base_folder on the external mount point in the root folder of the ftp user
             else
-                echo [ "$timestamp" ]: EXTERNAL DRIVE IS NOT MOUNTED
+                echo [ "$(date +"%F %T")" ]: EXTERNAL DRIVE IS NOT MOUNTED
             fi
 
             if $ext_dr_mnt_status && [[ $user_home != $ext_dr_mnt_pt ]]; then # Change destination_path if the external mount point is mounted, and is different from the mount point of the users home
 
-                echo [ "$timestamp" ]: CREATE DIRECTORY  mkdir -p "$ext_dr_mnt_pt""$base_folder""$rel_path"
+                echo [ "$(date +"%F %T")" ]: CREATE DIRECTORY  mkdir -p "$ext_dr_mnt_pt""$base_folder""$rel_path"
                 mkdir -p "$ext_dr_mnt_pt""$base_folder""$rel_path" # Create the directory
 
                 if [ -d "$ext_dr_mnt_pt""$base_folder""$rel_path" ]; then # Check if the newly created path exists before pointing variables to it
                     destination_path="$ext_dr_mnt_pt""$base_folder""$rel_path"
                     destination_file="$ext_dr_mnt_pt""$base_folder""$file_at_rel_path"
                 else
-                    echo [ "$timestamp" ]: NOT CHANGED DESTINATION PATH OR DESTINATION FILE
+                    echo [ "$(date +"%F %T")" ]: NOT CHANGED DESTINATION PATH OR DESTINATION FILE
                 fi
             fi
  	    ## Get the path to the destination file - End
@@ -98,7 +98,7 @@ tail -f -s 5 -n 1 /var/log/vsftpd.log | while read log_line; do
 
             convert_H264_to_H265 "$user_home""$file_at_rel_path" "$destination_file" & # Run the conversion as a separate process
         else
-            echo [ "$timestamp" ]: NOT AN MP4 FILE AT "$user_home""$file_at_rel_path"
+            echo [ "$(date +"%F %T")" ]: NOT AN MP4 FILE AT "$user_home""$file_at_rel_path"
         fi
     fi
 done
